de4ec61570f073c3f843dd0791f44df49d4d3c64
Roberto Guerra <prosaka@hotmail.com>
2023-08-12 00:50:11 -0300

    feat: add clarity

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,24 +9,22 @@ import "lib/styles/globals.css";
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <Chakra>
-      <Head>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
-        />
-        <script
-          // eslint-disable-next-line
-          dangerouslySetInnerHTML={{
-            __html: `
-            <script type="text/javascript">
+      <script
+        // eslint-disable-next-line
+        dangerouslySetInnerHTML={{
+          __html: `
                 (function(c,l,a,r,i,t,y){
                     c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                     t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                     y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
                 })(window, document, "clarity", "script", "idvsv9xoso");
-            </script>
             `,
-          }}
+        }}
+      />
+      <Head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
         />
       </Head>
 
