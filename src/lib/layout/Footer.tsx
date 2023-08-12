34ab242602f641ad55fd6fdc2505e362d0757ebd
Roberto Guerra <prosaka@hotmail.com>
2023-08-11 23:30:08 -0300

    feat: remove link site

--- src/lib/layout/Footer.tsx
+++ src/lib/layout/Footer.tsx
@@ -1,18 +1,9 @@
-import { Flex, Link, Text } from "@chakra-ui/react";
+import { Flex, Text } from "@chakra-ui/react";
 
 const Footer = () => {
   return (
     <Flex as="footer" width="full" justifyContent="center">
-      <Text fontSize="sm">
-        {new Date().getFullYear()} -{" "}
-        <Link
-          href="https://robertoguerra.dev"
-          isExternal
-          rel="noopener noreferrer"
-        >
-          robertoguerra.dev
-        </Link>
-      </Text>
+      <Text fontSize="sm">{new Date().getFullYear()}</Text>
     </Flex>
   );
 };
